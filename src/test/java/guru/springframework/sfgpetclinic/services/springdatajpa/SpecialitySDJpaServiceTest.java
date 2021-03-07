a2ff1e7dae5ab9cd71426b51355723a5a4378bf2
gent20 <gentkastratio@gmail.com>
2021-03-07 17:29:47 +0100

    tested a method in bdd approach

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -2,15 +2,20 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.BDDMockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -87,6 +92,21 @@ class SpecialitySDJpaServiceTest {
 
         verify(specialtyRepository).findById(1L);
 
+    }
+
+    @DisplayName("Find a specialty by ID in BDD approach")
+    @Test
+    void findByIdBddTest(){
+        //given
+        Speciality speciality = new Speciality();
+        given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
+
+        //when
+        Speciality savedSpecialty = service.findById(1L);
+
+        //then
+        assertThat(savedSpecialty).isNotNull();
+        BDDMockito.then(specialtyRepository).should().findById(anyLong());
     }
 
     @Test
